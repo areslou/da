2f98b173267b0bfd2a4c4618fdebf6734c9e0ec3
Brendan Lou Millares <brendanlou8@gmail.com>
2026-02-11 12:09:13 +0800

    d

--- leap-year.js
+++ leap-year.js
@@ -1,9 +1,10 @@
 function leapYear() {
   var year = parseInt(document.getElementById("s2-input").value);
 
-  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-    document.getElementById("s2-out").textContent = year + " is a Leap Year.";
-  } else {
-    document.getElementById("s2-out").textContent = year + " is NOT a Leap Year.";
-  }
+
+if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+  alert(year + " is a Leap Year."); 
+} else {
+  alert(year + " is NOT a Leap Year."); 
+}
 }
